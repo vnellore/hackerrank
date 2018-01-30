465d0572a40185e7cd14b886dc2ab2e2d9a13a8c
Vamsi Nellore <vamsi.nellore@gmail.com>
2018-01-29 23:30:49 -0500

    fixed balanced brackets

--- ctci/balanced_brackets.py
+++ ctci/balanced_brackets.py
@@ -5,7 +5,7 @@ def is_matched(expression):
 
     left_stack = []
     valid = True
-    for i in expression:
+    for i in expression.strip():
         if i in left_brackets:
             left_stack.append(i)
         elif i in right_brackets:
@@ -17,6 +17,17 @@ def is_matched(expression):
                 valid = False
                 break
         else:
-            print('Invalid character in string')
+            print('Invalid character in string ' + expression)
+    
+    if len(left_stack) > 0:
+        valid = False
 
     return valid
+
+if __name__ == "__main__":
+    
+    print('balanced brackets')
+    #with open('bb_test79.txt') as f:
+    #    f.readline()
+    #    for line in f:        
+            #print(line, end='\n')
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from ctci import balanced_brackets
 
 if __name__ == "__main__":
-
-    t = 1
-    for a0 in range(t):
-        expression = '({(){}[]})[]'
-        if balanced_brackets.is_matched(expression) is True:
-            print("YES")
-        else:
-            print("NO")
+    
+    #with open('ctci/bb_test79.txt') as f:
+    #    f.readline()
+    #    for line in f:        
+            #print(line, end='\n')
+    expression = '{{}('
+    
+    if balanced_brackets.is_matched(expression) is True:
+        print("YES")
+    else:
+        print("NO")
